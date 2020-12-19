b3b87771e8c239fd1dc95476d8ebe711a37efc9f
Sai <snaadira@gmail.com>
2020-12-18 20:01:06 -0500

    updates: added JournalEntry component

---
+++ stillness-app/src/Components/JournalEntry/JournalEntry.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const JournalEntry = () => {
+
+  return (
+    <div>
+      <p>I want all entries as their own separate component</p>
+    </div>
+  )
+}
+
+export default JournalEntry;
