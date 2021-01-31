3c7f3f920a0a6793d36c49559ff1896eeeae878b
Sai <snaadira@gmail.com>
2021-01-31 15:11:37 -0500

    updates: Breather component currently working and updated css in this component/ fixed css in SoundButtoncomponent.

--- stillness-app/src/Components/Breather/Breather.js
+++ stillness-app/src/Components/Breather/Breather.js
@@ -4,8 +4,9 @@ import SoundButton from './Components/SoundButton/SoundButton'
 
 
 const Breather = () => {
+  const initialBreatheText = 'Get Ready!'
   //variable and function to set the breathe text to inhale/hold/exhale
-  const [breatheText, setBreatheText] = useState('Inhale')
+  const [breatheText, setBreatheText] = useState(initialBreatheText)
   // the total time the pointer goes around the circumfrence: 7 seconds
   const totalTime = 7500
   // the amount of time the circle expands on inhale: 2.8 seconds
@@ -14,25 +15,74 @@ const Breather = () => {
   const holdTime = totalTime / 5
 
   const breatheAnimation = () => {
-    if (breatheText !== 'Inhale') {
-      setBreatheText('Inhale')
-    }
+    setBreatheText('Inhale')
+
     setTimeout(() => {
       setBreatheText('Hold')
-    }, holdTime)
-    setTimeout(() => {
-      setBreatheText('Exhale')
+      setTimeout(() => {
+        setBreatheText('Exhale')
+      }, holdTime)
     }, breatheTime)
+    clearInterval(counterInterval)
   }
+
+  // the useEffect that was not working:
+  // it was being called multiple times which is why it was not behaving the way i wanted it to.
+  // useEffect(() => {
+  //   // equivalent to componentDidMount() and componentDidUpdate()
+  //   const animationTime = setInterval(breatheAnimation, totalTime)
+  //   // function that handles the umounting of the component and clears all resources
+  //   return () => {
+  //     // equivalent to componentWillUnmount()
+  //     clearInterval(animationTime)
+  //   }
+  // })
+
+
+  let counterInterval;
+  // function that handles the countdown before breathing begins
+  const initialBreatheCounter = () => {
+    setTimeout(() => {
+      let counter = 3
+      counterInterval = setInterval(() => {
+        setBreatheText(counter)
+        counter--
+      }, 1000)
+    }, 3500)
+
+  }
+
+  // triggers function every time something happens (like an update)
   useEffect(() => {
+    // componentDidMount() 
+    initialBreatheCounter()
     const animationTime = setInterval(breatheAnimation, totalTime)
+    // function that handles the umounting of the component and clears all resources
     return () => {
+      // componentWillUnmount()
       clearInterval(animationTime)
     }
-  })
+    // passing an empty array so that the useEffect is only run once. a way to define componentDidMount() so it runs only once
+  }, [])
+
+
+  // function that gets the class according the state of the breatheText variable
+  const getBreatherClass = () => {
+    if (breatheText === 'Inhale') {
+      return 'expand'
+    } else if (breatheText === 'Exhale') {
+      return 'shrink'
+    } else if (breatheText === initialBreatheText) {
+      return 'getready'
+    } else {
+      return 'hold'
+    }
+  }
+  // stores getBreatherClass() function
+  const breatherClass = getBreatherClass()
 
   return (
-    <div className="backgroundImage">
+    <div className="background-image">
       {/* navigation links: add behavior */}
       <nav >
         <ul className="navigation-links">
@@ -63,9 +113,9 @@ const Breather = () => {
 
       <h1 className="breather-h1">Breather</h1>
       {/* if breatheText is inhale then the container will expand if its on exhale the container will shrink. */}
-      <div className={`breather-container ${breatheText === "Inhale" ? "expand" : breatheText === 'Exhale' ? "shrink" : 'hold'}`}>
-        {/* <!-- this div represents the solid colour circle --> */}
-        <div className="circle"></div>
+      <div className={`breather-container ${breatherClass}`}>
+        {/* <!-- this div represents the solid color circle and the conditional logic: if breatheText variable is equal to 'Get Ready!' then display the initial-circle class, otherwise display the circle class--> */}
+        <div className={`${breatheText === initialBreatheText ? "initial-circle" : "circle"}`}></div>
 
         {/* breatheText is changed dynamically here */}
         <p id="text">{breatheText}</p>
--- stillness-app/src/Components/Breather/Components/SoundButton/SoundButton.js
+++ stillness-app/src/Components/Breather/Components/SoundButton/SoundButton.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import './SoundButton.css'
 
-//AUDIO BUTTON SOUNDS
+// sound list: it supports four different audio sounds
 const sounds = {
   Ambient: new Audio('https://freesound.org/data/previews/554/554220_9497060-lq.mp3'),
   Nature: new Audio('https://freesound.org/data/previews/524/524813_10859468-lq.mp3'),
   Ocean: new Audio(' https://freesound.org/data/previews/413/413326_7723777-lq.mp3'),
-  Dreamy: new Audio('https://freesound.org/data/previews/263/263467_3946286-lq.mp3')
+  Dreamy: new Audio('https://freesound.org/data/previews/263/263467_3946286-lq.mp3'),
 }
 
 const SoundButton = (props) => {
   const [isPlaying, setIsPlaying] = useState(false)
 
-  // function to to handle the button's click and calls toggleSound() to play or pause audio depending on the state of isPlaying variable
+  // function to handle the button's click and calls toggleSound() to play or pause audio depending on the state of isPlaying variable
   const handleClick = () => {
     return toggleSound()
   }
 
-  // toggling the sound depending on the value of isPlaying and contolling the state
+  // toggling the sound depending on the value of isPlaying and controlling the state
   const toggleSound = () => {
     if (isPlaying) {
       sounds[props.type].pause()
